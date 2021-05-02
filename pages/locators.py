2cc902d3333d571fc145ffc29724194324c06a29
NickMileshkin <mileshkin.nikita@yandex.ru>
2021-05-02 11:53:53 +0700

    update locators.py add new class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,14 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOG_IN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+   BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+   PRODUST_NAME = (By.CSS_SELECTOR, '#content_inner .product_main h1')
+   PRODUCT_PRICE = (By.CSS_SELECTOR, '#content_inner .product_main p')
+   ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, '#messages > .alert:nth-child(1) > .alertinner > strong ')
+   BASKET_PRICE = (By.CSS_SELECTOR, '#messages > .alert:nth-child(3) > .alertinner > p > strong ')
+
