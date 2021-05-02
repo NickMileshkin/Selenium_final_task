f5ab429ee79612a834c98251574a686d71b6c42d
NickMileshkin <mileshkin.nikita@yandex.ru>
2021-05-02 11:54:54 +0700

    add login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,7 +14,7 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOG_IN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
