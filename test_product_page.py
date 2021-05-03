2ff7b67f43d811e85becb199459dcfc8f28a7c04
NickMileshkin <mileshkin.nikita@yandex.ru>
2021-05-03 08:11:42 +0700

    update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -64,5 +64,5 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
